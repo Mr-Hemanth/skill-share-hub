8fd2ce8b86ae45bc6d1e60e58d3c4fd3e11d386c
Mr-Hemanth <140688314+Mr-Hemanth@users.noreply.github.com>
2024-11-29 23:45:12 +0530

    Initial commit

---
+++ src/SplashScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SplashScreen.css'; // Import the CSS for styling
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-container">
+      <div className="logo-text">
+        <p>skill</p>
+        <p>share</p>
+        <p>hub</p>
+      </div>
+    </div>
+  );
+};
+
+export default SplashScreen;
