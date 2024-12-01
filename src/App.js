bde6f2debe54f7bfccd05a1a6ba8fba1b7bb8760
Mr-Hemanth <140688314+Mr-Hemanth@users.noreply.github.com>
2024-12-01 23:35:51 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import SplashScreen from './SplashScreen'; // Import your SplashScreen component
 
+
 function App() {
   return (
     <div className="App">
       <SplashScreen /> {/* Render the splash screen */}
-      
+
     </div>
   );
 }
